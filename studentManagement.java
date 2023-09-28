2205e87502af988232fb2ae4bbf74c342df4e9c4
vinifigueiroa <vinicius@criatco.com>
2023-09-28 20:27:23 -0300

    Final Assigment

---
+++ studentManagement.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class studentManagement {
+
+    public static void main(String[] args) {
+
+    }
+}
+
+public class Student {
+
+    private String name;
+    private int ID;
+    private int age;
+    private int grade;
+
+    public Student (String name, int ID, int age, int grade) {
+
+        this.name = name;
+        this.ID = ID;
+        this.age = age;
+        this.grade = grade;
+    }
+
+
+}
